f9e6c3f6debc19e413c4a548b81fbf3d0f3fca2d
Quan Nguyen <quankidno24@gmail.com>
2017-06-04 23:17:12 +0700

    Quan_QuanLyThietBi
    Quanlythietbi

--- BTThucTapNhom/BTThucTapNhom/frmThietBi.cs
+++ BTThucTapNhom/BTThucTapNhom/frmThietBi.cs
@@ -18,6 +18,95 @@ namespace BTThucTapNhom
             InitializeComponent();
         }
 
-        
+        ThietBi tb = new ThietBi();
+        string ma_tb;
+
+        public void LoadData()
+        {
+            DataTable dt = new DataTable();
+            dt = tb.ShowTB();
+            dtgvTB.DataSource = dt;
+        }
+
+        private void frmThietBi_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void dtgvTB_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            for (int i = 0; i < dtgvTB.RowCount; i++)
+            {
+                dtgvTB.Rows[i].Cells[0].Value = i + 1;
+            }
+        }
+
+        private void dtgvTB_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            int dong;
+            dong = e.RowIndex;
+            ma_tb = dtgvTB.Rows[dong].Cells[1].Value.ToString();
+            this.txtMaTB.Text = dtgvTB.Rows[dong].Cells[1].Value.ToString();
+            this.txtMaPhongTB.Text = dtgvTB.Rows[dong].Cells[2].Value.ToString();
+            this.txtTenTB.Text = dtgvTB.Rows[dong].Cells[3].Value.ToString();
+            this.txtSoLuongTB.Text = dtgvTB.Rows[dong].Cells[3].Value.ToString();
+        }
+
+        private void frmThietBi_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void btnThemTB_Click(object sender, EventArgs e)
+        {
+            frmThemTB frmthemtb = new frmThemTB();
+            frmthemtb.FormClosed += frmThietBi_FormClosed;
+            frmthemtb.Show();
+        }
+
+        private void btnSuaTB_Click(object sender, EventArgs e)
+        {
+            if (txtMaTB.Text.Length == 0)
+                MessageBox.Show("Bạn chưa chọn mã thiết bị để sửa!");
+            else
+                if (txtMaTB.Text.Length > 5)
+                MessageBox.Show("Mã phòng không được dài quá 5 kí tự!");
+            else
+                if (txtMaPhongTB.Text.Length == 0)
+                MessageBox.Show("Tên loại phòng không được bỏ trống!");
+            else
+            {
+                tb.SuaTB(
+                ma_tb,
+                txtMaTB.Text,
+                txtMaPhongTB.Text,
+                txtTenTB.Text,
+                txtSoLuongTB.Text);
+                MessageBox.Show("Sửa thành công!");
+                LoadData();
+            }
+        }
+
+        private void btnXoaTB_Click(object sender, EventArgs e)
+        {
+            if (this.txtMaTB.TextLength == 0)
+                MessageBox.Show("Bạn cần chọn nhân viên để xóa");
+            else
+            {
+                tb.XoaTB(ma_tb);
+                MessageBox.Show("Đã xóa thành công!");
+                frmThietBi_Load(sender, e);
+            }
+        }
+
+        private void btnThoatTB_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Thông Báo !", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                frmChinh frmchinh = new frmChinh();
+                frmchinh.Show();
+                this.Hide();
+            }
+        }
     }
 }
--- BTThucTapNhom/BUS/ThietBi.cs
+++ BTThucTapNhom/BUS/ThietBi.cs
@@ -10,6 +10,31 @@ namespace BUS
 {
     public class ThietBi
     {
-        
+        Data da = new Data();
+        public DataTable ShowTB()
+        {
+            string sql = "select *from ThietBi";
+            DataTable dt = new DataTable();
+            dt = da.GetTable(sql);
+            return dt;
+        }
+
+        public void InsertTB(string MaTB, string MaPhong, string TenTB, int SoLuong)
+        {
+            string sql = "insert into ThietBi values ('" + MaTB + "', '" + MaPhong + "', N'" + TenTB + "', '" + SoLuong + "')";
+            da.NonQuery(sql);
+        }
+
+        public void SuaTB(string ma_tb, string MaTB, string MaPhong, string TenTB, string SoLuong)
+        {
+            string sql = "update ThietBi set MaTB='" + MaTB + "', TenTB=N'" + TenTB + "', SoLuong='" + SoLuong + "' where MaTB='" + ma_tb + "'";
+            da.NonQuery(sql);
+        }
+
+        public void XoaTB(string MaTB)
+        {
+            string sql = "delete ThietBi where MaTB='" + MaTB + "'";
+            da.NonQuery(sql);
+        }
     }
 }
